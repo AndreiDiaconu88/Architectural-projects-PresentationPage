4c09633854eb1ffa96524efdc7cbefd24529c862
Andrei Diaconu <andrei_diaconu88@yahoo.com>
2018-07-19 14:23:26 +0000

    commit message

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// var p = prompt("merge!")
+
+var $item = $('.carousel-item');
+var $wHeight = $(window).height();
+var $wWidth = $(window).width();
+
+// $item.eq(0).addClass('active');
+$item.height($wHeight);
+$item.width($wWidth);
+$item.addClass('full-screen');
+
+$('.carousel img').each(function() {
+  var $src = $(this).attr('src');
+  $(this).parent().css({
+    'background' : 'url(' + $src + ')'
+  });
+  $(this).remove();
+});
+// $item.eq(0).addClass('active');
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $wWidth = $(window).width();
+  $item.height($wHeight);
+  $item.width($wWidth);
+});
